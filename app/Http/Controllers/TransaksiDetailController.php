c2949c94b686da092a3485d99dead9444e74d736
Fagan <mustafafagan@gmail.com>
2024-12-03 14:45:16 +0700

    bugfix #20 #21

--- app/Http/Controllers/TransaksiDetailController.php
+++ app/Http/Controllers/TransaksiDetailController.php
@@ -44,11 +44,12 @@ class TransaksiDetailController extends Controller
             $transaksidetail->nama_produk = $request->input('nama_produk');
             $transaksidetail->harga_satuan = $request->input('harga_satuan');
             $transaksidetail->jumlah = $request->input('jumlah');
+            $subtotal_old = $transaksidetail->subtotal;
             $transaksidetail->subtotal = $request->input('harga_satuan') * $request->input('jumlah');
 
             $transaksi = Transaksi::with('transaksidetail')->findOrFail($transaksidetail->id_transaksi);
 
-            // $transaksi->total_harga = $transaksi->total_harga - ;
+            $transaksi->total_harga = ($transaksi->total_harga - $subtotal_old) + $transaksi->subtotal;
             $transaksi->kembalian = $transaksi->bayar - $transaksi->total_harga; // hapus rumus
 
             return redirect('transaksidetail/'.$transaksidetail->id_transaksi)->with('pesan', 'Berhasil mengubah data');
@@ -63,9 +64,11 @@ class TransaksiDetailController extends Controller
         $transaksidetail = TransaksiDetail::findOrFail($id);
 
         $transaksi = Transaksi::with('transaksidetail')->findOrFail($transaksidetail->id_transaksi);
-        // $transaksi->total_harga = sum subtotal;
-        $transaksi->kembalian = $transaksi->bayar - $transaksi->total_harga;;
+        $transaksi->total_harga = $transaksi->total_harga - $transaksidetail->subtotal;
+        $transaksi->kembalian = $transaksi->bayar - $transaksi->total_harga;
         $transaksi->save();
+
+        $transaksidetail->delete();
 
         return redirect('transaksidetail/'.$transaksidetail->id_transaksi)->with('pesan', 'Berhasil menghapus data');
     }
